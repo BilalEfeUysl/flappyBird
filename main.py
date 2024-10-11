54e87be8390ce82ec330101c0f4f30df800adcd3
BilalEfeUysl <bilalefeuysalbs@gmail.com>
2024-10-11 21:43:54 +0300

    adding the screen and the main frame

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print("hello world")
+import pygame
+
+pygame.init()
+
+# these will determine the size of the screen
+width = 500
+height = 750
+
+# defining the main settings of the game
+screen = pygame.display.set_mode(width,height)
+pygame.display.set_caption("Flappy Bird")
+clock = pygame.time.Clock()
+
+
+flag = True
+while flag:
+
+    # to exit the game
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            flag = False
+
+    # for updating the screen
+    pygame.display.update()   
+    
+    # fps
+    clock.tick(144)
+
+
