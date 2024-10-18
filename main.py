feda703adfc53602299cb49d2b6e4d8a3d36e6be
BilalEfeUysl <bilalefeuysalbs@gmail.com>
2024-10-18 18:17:03 +0300

    starting and ending texts

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ pipe_width = 70
 pipe_height = random.randint(150,250)
 pipe_color = (0,255,0)
 pipe_x = 400
-pipe_x_change = -2
+pipe_x_change = -1  
 
 # function of creating pipe
 def display_pipe(height):
@@ -46,6 +46,28 @@ def display_pipe(height):
 
     pygame.draw.rect(screen,pipe_color,(pipe_x,bottom_pipe_y,pipe_width,(550-bottom_pipe_y)))
 
+# while loop to wait on start screen    
+wait = True
+while wait:
+    # add background to screen 
+    screen.blit(background,(0,0))
+
+    font = pygame.font.Font(None, 45)
+
+    welcome_text = font.render("PRESS THE SPACE", True, (0,0,0))
+
+    screen.blit(welcome_text,(70,70))
+
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN: 
+            if event.key == pygame.K_SPACE:
+                wait = False
+        if event.type == pygame.QUIT:
+            wait = False    
+
+    pygame.display.update()
+
+game_over = True
 flag = True
 while flag:
 
@@ -55,7 +77,8 @@ while flag:
     for event in pygame.event.get():
         # to exit the game
         if event.type == pygame.QUIT:
-            flag = False    
+            flag = False
+            game_over = False
         # when the button pressed 
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
@@ -63,7 +86,7 @@ while flag:
         # when the button is not pressed
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_SPACE:
-                bird_y_change = 2
+                bird_y_change = 1
 
     # update coordinate 
     bird_y += bird_y_change
@@ -98,6 +121,27 @@ while flag:
     
     # fps
     clock.tick(144)
+
+# creating the game over text
+wait = True
+while wait:
+
+    font = pygame.font.Font(None, 60)
+
+    game_over_text = font.render("GAME OVER", True, (255, 105, 180))
+
+    screen.blit(game_over_text,(75,250))
+
+    # to exit the game
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN: 
+            if event.key == pygame.K_SPACE:
+                wait = False
+        if event.type == pygame.QUIT:
+            wait = False    
+
+    pygame.display.update()
+
 
 #quit the program
 pygame.quit()
